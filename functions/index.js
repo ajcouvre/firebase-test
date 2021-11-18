2e97cadb1d9142ee136aaa61ae52f4bf641c7c75
ajcouvre <ajcouvre@gmail.com>
2021-11-17 18:11:05 -0800

    Demo for Github Actions

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,6 @@ const functions = require('firebase-functions')
 module.exports = {
     hello: functions.https.onRequest((req, res) => {
         // const subject = req.query.subject || 'World'
-        res.send('Github Actions Worked')
+        res.send('Test Demo Github actions')
     }),
 }
