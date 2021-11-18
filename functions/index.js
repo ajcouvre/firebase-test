138a62a2a0dd92d20ddf0e148d77c5117ff2843f
ajcouvre <ajcouvre@gmail.com>
2021-11-17 18:34:52 -0800

    Testing single function deployment

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,9 @@ const functions = require('firebase-functions')
 module.exports = {
     hello: functions.https.onRequest((req, res) => {
         // const subject = req.query.subject || 'World'
-        res.send('Test Demo Github actions')
+        res.send('Test Did Not Deploy')
     }),
+    goodbye: functions.https.onRequest((req, res) => {
+        res.send('Goodbye from a single function')
+    })
 }
