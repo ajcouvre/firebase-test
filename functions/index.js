fe3fb0f851494ebdb91c6b7b235517bb697e2ff0
ajcouvre <ajcouvre@gmail.com>
2021-11-17 18:41:10 -0800

    Try #2 single function dpeloyment

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const functions = require('firebase-functions')
 module.exports = {
     hello: functions.https.onRequest((req, res) => {
         // const subject = req.query.subject || 'World'
-        res.send('Test Did Not Deploy')
+        res.send('Test Should Not Deploy')
     }),
     goodbye: functions.https.onRequest((req, res) => {
         res.send('Goodbye from a single function')
